81a4ec747c1db5a2e245f7003eefc41626c6b3f1
Eduardo <edo.barrag@gmail.com>
2016-11-30 23:09:03 -0300

    Commit desde eclipse

---
+++ src/main/java/cl/lucas/main/HolaMundo.java
@@ -0,0 +1,11 @@
+package cl.lucas.main;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo Maven!..."); // Impresion en Consola
+		System.out.println("Bienvenido al Club del Programador!..."); // Impresion
+		// en
+		// Consola
+		}
+}
