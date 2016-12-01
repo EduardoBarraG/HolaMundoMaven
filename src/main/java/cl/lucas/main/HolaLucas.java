6b5c712238b22cb1f077bf08a187cdb886c1f52e
Eduardo <edo.barrag@gmail.com>
2016-11-30 23:10:10 -0300

    Commit de la clase HolaLucas

---
+++ src/main/java/cl/lucas/main/HolaLucas.java
@@ -0,0 +1,15 @@
+
+package cl.lucas.main; 
+
+public class HolaLucas {
+	private int lucas;
+
+	public int getLucas() {
+		return lucas;
+	}
+
+	public void setLucas(int lucas) {
+		this.lucas = lucas;
+	}
+	
+}
