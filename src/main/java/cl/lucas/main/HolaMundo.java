94fa268bd9ef5be7debbbf39c9561a9275908360
Eduardo <edo.barrag@gmail.com>
2016-12-01 10:21:12 -0300

    mensaje del master

--- src/main/java/cl/lucas/main/HolaMundo.java
+++ src/main/java/cl/lucas/main/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo Maven!..."); // Impresion en Consola
-		System.out.println("Bienvenido al Club del Programador!..."); // Impresion
+		System.out.println("Bienvenido al Club del Master!..."); // Impresion
 		// en
 		// Consola
 		}
